1cce7f6a0941a6f880769bd22da6e8dd24ebba58
Ken Pierce <kpierce8@gmail.com>
2015-09-17 23:59:42 +0000

    got server response and a form at /other using handlebars

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
+
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
 
 
 app.get('/', function(req, res, next){
    res.send("i'm waiting for something to do"); 
 });
 
+app.get('/other', function(req, res, next){
+    var vm = {
+        title: "A cool title from model",
+        meme: "you can't just walk into mordor"
+    };
+   res.render('index', vm); 
+   
+});
 
-app.listen(process.env.PORT ||3100);
 
-console.log('started up on');
+
+var server = app.listen(process.env.PORT ||3100, function(){
+    console.log('started up on');
+});
+
