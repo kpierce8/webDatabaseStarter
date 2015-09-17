8f1e6be279d04976d4c87483ad5cc87fef100003
Ken Pierce <kpierce8@gmail.com>
2015-09-17 20:25:29 +0000

    simplest web server responds to simplest route

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var http = require('http');
 
 var app = express();
 
 
+app.get('/', function(req, res, next){
+   res.send("i'm waiting for something to do"); 
+});
 
 
-app.listen(3100);
+app.listen(process.env.PORT ||3100);
 
 console.log('started up on');
