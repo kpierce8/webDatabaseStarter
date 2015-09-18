13095265cce472ab558a0cdd65c813af68aaeb01
Ken Pierce <kpierce8@gmail.com>
2015-09-18 06:05:40 +0000

    got form submission going, forgot to add body-parser, thank you stack overflow

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var bodyParser = require('body-parser');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +21,7 @@ app.set('view engine', 'hbs');
 
 
 app.use('/', routes);
-app.use('/users', users)
+app.use('/users', users);
 
 
 var server = app.listen(process.env.PORT ||3100, function(){
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,23 @@ var router = express.Router();
 router.get('/create', function(req, res, next) {
     var vm = {
         title: "Create a user"
+    };
+   res.render('users/create', vm); 
+});
+
+router.post('/create', function(req, res, next) {
+    var somethingGoesWrong = true;
+    if (somethingGoesWrong) {
+         var vm = {
+        title: "Create a user",
+        input: req.body,
+        error: 'something went wrong'
+    };
+    delete vm.input.password;
+    return res.render('users/create', vm); 
     }
-   res.render('/users/create', vm); 
+   
+   res.redirect('../other');
 });
 
 
