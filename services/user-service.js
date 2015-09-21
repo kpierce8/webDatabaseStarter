a5ca1117a3a6d21dba36f08b741d4fec1f5e8cf4
Ken Pierce <kpierce8@gmail.com>
2015-09-21 00:55:18 +0000

    bcrypt hashes password but redirect to colors after new account creation not working

--- services/user-service.js
+++ services/user-service.js
@@ -1,25 +1,36 @@
 var User = require('../models/users').User;
-
+var bcrypt = require('bcrypt');
 
 
 exports.addUser = function(user, next) {
-  var newUser = new User({
-     email: user.email.toLowerCase(),
-     password: user.password
-  });  
+    bcrypt.hash(user.password, 10, function(err, hash) {
+        if (err) {
+            return next(err);
+        }
 
 
-newUser.save(function(err){
-   if (err) {
-       return next(err);
-   }
-   return next(null);
-});
+        var newUser = new User({
+            email: user.email.toLowerCase(),
+            password: hash
+        });
+
+
+        newUser.save(function(err) {
+            if (err) {
+                return next(err);
+            }
+            return next(null);
+        });
+    });
+
+
 };
 // added to check for existing user during create account  
 exports.findUser = function(email, next) {
-    User.findOne({email: email.toLowerCase()}, function(err, user) {
-     next(err, user);
+    User.findOne({
+        email: email.toLowerCase()
+    }, function(err, user) {
+        next(err, user);
     });
 
 };
