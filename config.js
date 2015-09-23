717f41fe819f4da8c53308b00467b47bf21af822
Ken Pierce <kpierce8@gmail.com>
2015-09-23 20:46:25 +0000

    added mongolab string

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 var config = {};
 
-config.mongoUri = "mongodb://localhost:27017/favcolor";
+//config.mongoUri = "mongodb://localhost:27017/favcolor";
+
+config.mongoUri = "mongodb://adminTest:urlic8@ds051863.mongolab.com:51863/heroku_wvh6hn5n";
 
 module.exports = config;
